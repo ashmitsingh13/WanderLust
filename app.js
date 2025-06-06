6894b67503ce4abda1bbbb2ea069f83a771983be
Ashmit Singh <ashmitsingh1310@gmail.com>
2025-06-06 21:29:11 +0530

    Add New Features

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const ejs = require("ejs");
 const Listing = require("./models/listing.js");
 const path = require('path')
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
+app.engine('ejs', ejsMate);
 app.use(methodOverride('_method'));
 
 main().then(() => {
@@ -21,7 +24,7 @@ app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
